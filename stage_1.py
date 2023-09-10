ebb6a7b4288f8fef80ba217cf0b8d784af8bb462
Maxidonx <johnmaxwell927@gmail.com>
2023-09-10 20:07:06 +0100

    utc time changes

--- stage_1.py
+++ stage_1.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify
-from datetime import datetime, timedelta
+from datetime import datetime
 import pytz
 
 app = Flask(__name__)
+
+# Function to validate UTC offset (+/- 2 hours)
+def validate_utc_offset(utc_offset):
+    try:
+        offset = int(utc_offset)
+        return abs(offset) <= 2
+    except ValueError:
+        return False
 
 @app.route('/api', methods=['GET'])
 def get_info():
@@ -16,19 +24,22 @@ def get_info():
 
     # Get current UTC time
     current_time = datetime.now(pytz.utc)
-
+    
     # Get the current day of the week
     current_day = current_time.strftime("%A")
+    
 
     # Use your actual GitHub URLs here
     github_url_file = "https://github.com/Maxidonx/HNG-Task-1/blob/master/stage_1.py"
     github_url_source = "https://github.com/Maxidonx/HNG-Task-1.git"
 
+    
+    
     # Create the response JSON
     response_data = {
         "slack_name": slack_name,
         "current_day": current_day,
-        "utc_time": current_time.strftime("%Y-%m-%d %H:%M:%S %Z"),
+        "utc_time": current_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "track": track,
         "github_file_url": github_url_file,
         "github_repo_url": github_url_source,
